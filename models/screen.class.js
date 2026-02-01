9cab03f0a3375dcd8ef56b7983e4b5b862c7f3b7
David Groß <d.g1r2o3s4s@gmail.com>
2026-02-01 17:08:02 +0100

    edit screen.class.js

--- models/screen.class.js
+++ models/screen.class.js
@@ -1,35 +1,36 @@
 class Screen extends DrawableObject {
     IMAGES = {
         END_LOST: [
-            'img/9_intro_outro_screens/game_over/game over!.png',
-            'img/9_intro_outro_screens/game_over/game over.png',
-            'img/9_intro_outro_screens/game_over/oh no you lost!.png',
-            'img/9_intro_outro_screens/game_over/you lost.png',
-            'img/9_intro_outro_screens/game_over/game over!.png',
-            'img/9_intro_outro_screens/game_over/game over.png',
-            'img/9_intro_outro_screens/game_over/oh no you lost!.png',
-            'img/9_intro_outro_screens/game_over/you lost.png',
-            'img/9_intro_outro_screens/game_over/game over!.png',
-            'img/9_intro_outro_screens/game_over/game over.png'
+            './img/9_intro_outro_screens/game_over/game over!.png',
+            './img/9_intro_outro_screens/game_over/game over.png',
+            './img/9_intro_outro_screens/game_over/oh no you lost!.png',
+            './img/9_intro_outro_screens/game_over/you lost.png',
+            './img/9_intro_outro_screens/game_over/game over!.png',
+            './img/9_intro_outro_screens/game_over/game over.png',
+            './img/9_intro_outro_screens/game_over/oh no you lost!.png',
+            './img/9_intro_outro_screens/game_over/you lost.png',
+            './img/9_intro_outro_screens/game_over/game over!.png',
+            './img/9_intro_outro_screens/game_over/game over.png',
+            './img/9_intro_outro_screens/game_over/oh no you lost!.png'
         ],
         END_WIN: [
-            'img/You won, you lost/You Win A.png',
-            'img/You won, you lost/You win B.png',
-            'img/You won, you lost/You won A.png',
-            'img/You won, you lost/You Won B.png',
-            'img/You won, you lost/You Win A.png',
-            'img/You won, you lost/You win B.png',
-            'img/You won, you lost/You won A.png',
-            'img/You won, you lost/You Won B.png',
-            'img/You won, you lost/You Win A.png',
-            'img/You won, you lost/You win B.png'
+            './img/You won, you lost/You Win A.png',
+            './img/You won, you lost/You win B.png',
+            './img/You won, you lost/You won A.png',
+            './img/You won, you lost/You Won B.png',
+            './img/You won, you lost/You Win A.png',
+            './img/You won, you lost/You win B.png',
+            './img/You won, you lost/You won A.png',
+            './img/You won, you lost/You Won B.png',
+            './img/You won, you lost/You Win A.png',
+            './img/You won, you lost/You win B.png',
+            './img/You won, you lost/You won A.png'
         ],
         START: [
-            'img/9_intro_outro_screens/start/startscreen_1.png',
-            'img/9_intro_outro_screens/start/startscreen_2.png',
+            './img/9_intro_outro_screens/start/startscreen_1.png',
+            './img/9_intro_outro_screens/start/startscreen_2.png',
         ]
     };
-    random = 0;
 
     constructor(arr) {
         super();
@@ -41,7 +42,7 @@ class Screen extends DrawableObject {
         if(arr === 'END_LOST' || arr === 'END_WIN') {
             this.random = Math.round(Math.random() * 10);
         } else if (arr === 'START') {
-            this.random = Math.round(Math.random()*10) % 2;
+            this.random = Math.round(Math.random()*10) % 2; 
         }
         this.loadImage(this.IMAGES[arr][this.random]);
     }
